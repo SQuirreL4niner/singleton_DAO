970713bf14fee5612068ec3eede1ca7b2aa98966
SQuirreL4niner <eprojex.network@gmail.com>
2015-02-27 00:43:22 -0500

    First Commit

---
+++ src/main/java/net/eprojex/DAO/App.java
@@ -0,0 +1,13 @@
+package net.eprojex.DAO;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
